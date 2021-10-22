270b3632c6a6efbfff10dbfb23d7283e3bc8d564
Agustin <agualvila@gmail.com>
2021-10-22 11:32:17 -0300

    fix db config

--- src/database/config.js
+++ src/database/config.js
@@ -1,14 +1,3 @@
-/** LOCAL MySQL server */
-/* const database = {
-    username: 'root',
-    password: '',
-    database: 'disney_world',
-    host: 'localhost',
-    Port: 3306,
-    dialect: 'mysql'
-}; */
-
-/** Remote MySQL */
 const database = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
